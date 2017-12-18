c87238c9b0b78e7dc1b67abec981663576a98ce0
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2017-12-18 17:00:07 +0530

    Add POST Route for replying to post
    - Add POST Route to reply to a post(not to another reply)
    - Change Discusion Page to show Replies as an unordered list

--- routes/discussion.js
+++ routes/discussion.js
@@ -24,5 +24,34 @@ route.get('/:pollId', (req, res)=>{
         });
 });
 
+// POST Route for Replying to Discussion(not to another reply)
+route.post('/:pollId/replies', (req,res)=>{
+    // Find the poll to reply on
+    models.Poll.findById(req.params.pollId)
+    .then((poll)=>{
+        // Create new Reply
+        models.Reply.create({
+            sender: req.user._id,
+            body: req.body.body
+        })
+        .then((reply)=>{
+            // Add the new reply to poll's Replies
+            poll.replies.push(reply._id);
+            return poll.save();
+        })
+        .then((poll)=>{
+            // Redirect to Discussion Page if succesfully Replied
+            res.redirect(`/discussions/${poll._id}`);
+        })
+    })
+    .catch((err)=>{
+        // Else redirect User to Index Page
+        console.log("Error: " + err);
+        res.redirect('/');
+    });
+});
+
+
+
 // Export the Router
 module.exports = route;
