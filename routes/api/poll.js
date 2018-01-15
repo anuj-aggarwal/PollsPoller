e3d089c0de9359f596b51f8677fee754c4f292e2
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2018-01-15 15:10:09 +0530

    Change Polls Page to be rendered at server side
    - Render All Polls Page at server side
    - Change Route for all polls to /polls from /polls/show
    - Format routes/polls.js

--- routes/api/poll.js
+++ routes/api/poll.js
@@ -11,38 +11,6 @@ const { checkAPILoggedIn } = require("../../helpers");
 //       ROUTES
 //--------------------
 
-// GET Route for all polls
-route.get("/", (req, res) => {
-	// Decide method for sorting(trending/recent/default)
-	// from query parameter "sort"
-	let sortBy;
-	switch (req.query.sort) {
-		case "recent":
-			// recent sorted by last created
-			sortBy = "createdAt";
-			break;
-		case "trending":
-			// trending sorted by number of votes
-			sortBy = "voteCount";
-			break;
-		case "default":
-			// Default sorting by last updated
-			sortBy = "updatedAt";
-			break;
-	}
-
-	// Get all the polls with question, author and voteCount only
-	models.Poll.find({}, "question author voteCount")
-	// Sort the polls according to sorting method
-	      .sort({ [sortBy]: "descending" })
-	      // Populate the username of the author
-	      .populate("author", "username")
-	      // Send the Polls to user
-	      .then(polls => res.send(polls))
-	      .catch(console.log);
-});
-
-
 // POST Route for Creating new Poll
 route.post("/", checkAPILoggedIn, (req, res) => {
 	// Create a new Poll
