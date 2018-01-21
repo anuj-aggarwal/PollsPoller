dfc79d35193811d2ac5aa55e74d1196cc24ad309
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2018-01-21 19:17:20 +0530

    Add Delete Poll Funcionality
    - Add Frontend for Deleting a Poll(Button + Event Handler to send Delete
    Request)
    - Add Route for Deleting a Poll and all its Replies with their Children
    Replies

--- routes/api/poll.js
+++ routes/api/poll.js
@@ -38,5 +38,67 @@ route.post("/", checkAPILoggedIn, (req, res) => {
 });
 
 
+// Delete Route for Poll
+route.delete("/:id", checkAPILoggedIn, (req, res) => {
+	let replies = [];
+
+	// Find the Poll
+	models.Poll.findById(req.params.id)
+	      .then(poll => {
+		      // If Author is the same as Current User
+		      if (poll.author.toString() !== req.user._id.toString()) {
+			      res.send({ err: "Can't Delete Other User's Poll" });
+			      throw new Error("Invalid Access!");
+		      }
+
+		      // Save the Replies of User for Delete
+		      replies = poll.replies;
+
+		      // Delete the Poll
+		      return poll.remove();
+	      })
+	      // Send the Poll to user
+	      .then(poll => {
+		      console.log(`Deleted: ${poll}`);
+		      res.send(poll);
+	      })
+	      // Remove all replies with their Children Replies
+	      .then(() => {
+		      return deleteReplyAndChildren(replies);
+	      })
+	      .then(() => {
+		      console.log("Successfully Deleted all replies of Poll: " + req.params.id);
+	      })
+	      .catch(console.log);
+});
+
+
+// Function to Delete all Replies in replies and their Children Replies
+// Uses: deleteReplyAndChildren()
+function deleteReplies(replies) {
+	let promises = [];
+
+	// For each reply, Delete it with its children Replies
+	replies.forEach(reply => promises.push(deleteReplyAndChildren(reply)));
+
+	return Promise.all(promises);
+}
+
+// Function to delete a Reply and all its children
+function deleteReplyAndChildren(replyId) {
+	// Find the reply
+	return models.Reply.findById(replyId)
+	             .then(reply => {
+		             // Delete Inner(Children) Replies
+		             let promises = reply.replies.map(reply => deleteReplyAndChildren(reply));
+		             return Promise.all(promises);
+	             })
+	             .then(() => {
+		             return models.Reply.findByIdAndRemove(replyId);
+	             })
+	             .then(reply => console.log("Deleted: " + reply));
+}
+
+
 // Export the Router
 module.exports = route;
